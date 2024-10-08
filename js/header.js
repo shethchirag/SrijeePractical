5c3849bf79d005f269a7d141e9f23cc471d98e51
sheth chirag <102785098+shethchirag@users.noreply.github.com>
2024-10-08 13:05:21 +0530

    final changes

--- js/header.js
+++
@@ -1,9 +0,0 @@
-export function changeNavbarBackground() {
-  let navbar = document.querySelector(".navbar_style");
-
-  if (window.scrollY > 780) {
-    navbar.style.backgroundColor = "#0000008f";
-  } else {
-    navbar.style.backgroundColor = "transparent";
-  }
-}
